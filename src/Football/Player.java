d5bebcac61909cd7df9b657110ad8a703dca600b
David Alves <davidalves04@hotmail.com>
2025-04-30 11:00:06 +0100

    2

---
+++ src/Football/Player.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Football;
+import enums.Remate;
+import enums.Passe;
+import enums.Velocidade;
+
+/*  
+* Nome: David Sérgio Ferreira Alves  
+* Número: 8240231  
+* Turma: LSIRC T2  
+*  
+* Nome: Gabriel Moreira
+* Número: 8240266   
+* Turma: LSIRC T2
+*/
+public class Player {
+    private String nome;
+    private int posicao;
+    private Remate remate;
+    private Passe passe;
+    private Velocidade velocidade;
+}
---
+++ src/Football/Team.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Football;
+
+/*  
+* Nome: David Sérgio Ferreira Alves  
+* Número: 8240231  
+* Turma: LSIRC T2  
+*  
+* Nome: Gabriel Moreira
+* Número: 8240266   
+* Turma: LSIRC T2
+*/
+public class Team {
+    
+}
