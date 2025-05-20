30ea38de7a683ac8a1d3968861e1ee7c4eda8160
Gaa56 <gabrielexmoreira@gmail.com>
2025-05-20 16:02:13 +0100

    JSONImporter

--- src/api/data/JSONImporter.java
+++ src/api/data/JSONImporter.java
@@ -4,10 +4,30 @@
  */
 package api.data;
 
+import com.fasterxml.jackson.databind.ObjectMapper; //Biblioteca para dar import ou export de JSON
+import java.io.File;
+import java.io.IOException;
+
+
 /**
  *
  * @author david
  */
 public class JSONImporter {
-    
+
+    private ObjectMapper mapper;
+
+    public JSONImporter() {
+        this.mapper = new ObjectMapper();
+    }
+
+        public void ImportJSON(String caminho, Class classe) {
+        try {
+            Object obj = mapper.readValue(new File(caminho), classe);
+            System.out.println("Objeto importado: " + obj);
+        } catch (IOException e) {
+            e.printStackTrace(); // ou logar de outra forma
+        }
+    }
+
 }
