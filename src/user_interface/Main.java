9d1045b55692c5849d369b52649616deea319a7b
Gaa56 <gabrielexmoreira@gmail.com>
2025-05-28 09:59:54 +0100

    main

--- src/user_interface/Main.java
+++ src/user_interface/Main.java
@@ -1,6 +1,6 @@
 package user_interface;
 
-import com.ppstudios.footballmanager.api.contracts.league.ILeague;
+
 import com.ppstudios.footballmanager.api.contracts.league.ISeason;
 import com.ppstudios.footballmanager.api.contracts.league.IStanding;
 import com.ppstudios.footballmanager.api.contracts.team.IClub;
@@ -11,28 +11,37 @@ import api.simulation.DefaultMatchSimulator;
 import api.simulation.LeagueSimulator;
 import api.league.League; // Adiciona o import para League
 
+
+
 public class Main {
     public static void main(String[] args) {
         try {
             // Caminho para o ficheiro JSON com os dados da liga
-            String jsonPath = "resources/json/liga.json"; // <- ajusta se necessário
+            String jsonPath = "league.json"; // <- ajusta se necessário
 
             // Importar ligas do ficheiro JSON
-            League[] ligas = LeagueImporterJSON.leaguesFromJson(jsonPath);
-
+            League liga = LeagueImporterJSON.readLeagueFromFile(jsonPath);
+             
+            
+            
             // Verificar se há pelo menos uma liga
-            if (ligas.length == 0) {
+            if (liga == null) {
                 System.out.println("Nenhuma liga encontrada no ficheiro JSON.");
                 return;
             }
 
-            // Usar a primeira liga
-            ILeague liga = ligas[0];
+           
+            
 
             // Obter a primeira temporada e a equipa do primeiro classificado
             ISeason season = liga.getSeason(0);
+            
+            
+             
+            
             IStanding[] standings = season.getLeagueStandings();
             IClub meuClube = standings[0].getTeam().getClub();
+            
 
             // Inicializar estratégia de simulação e simulador
             MatchSimulatorStrategy strategy = new DefaultMatchSimulator();
