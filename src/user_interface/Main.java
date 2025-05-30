87c5270b0bd9014a80fc2dccd8fcbdbd247a26a7
David Alves <davidalves04@hotmail.com>
2025-05-31 00:06:55 +0100

    update

--- src/user_interface/Main.java
+++ src/user_interface/Main.java
@@ -14,77 +14,70 @@ import api.data.LeagueImporterJSON;
 import api.data.TeamImporterJSON;
 import api.simulation.DefaultMatchSimulator;
 import api.simulation.LeagueSimulator;
-import api.league.League; // Adiciona o import para League
+import api.league.League;
 import api.league.Season;
 import api.team.Squad;
 import api.team.Team;
 import com.ppstudios.footballmanager.api.contracts.league.ISeason;
 
-
-import com.ppstudios.footballmanager.api.contracts.league.ISeason;
-
-
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Classe principal responsável por inicializar o simulador de futebol.
+ * <p>
+ * Esta classe carrega os dados das equipas e da liga a partir de ficheiros JSON,
+ * inicializa a época e o simulador, e apresenta o menu principal ao utilizador.
+ * </p>
+ */
 public class Main {
+    /**
+     * Método principal que serve como ponto de entrada da aplicação.
+     *
+     * @param args Argumentos da linha de comandos (não utilizados).
+     */
     public static void main(String[] args) {
         try {
-         
-            // Caminho para o ficheiro JSON com os dados da liga
+            // Caminhos para os ficheiros JSON com os dados
+            String leagueFile = "LigaPortugal.json"; // Caminho para a liga
+            String clubsFile = "clubs.json";         // Caminho para os clubes
+            String squadsFile = "squad.json";        // Caminho para os plantéis
+            String mySquadFile = "mySquad.json";     // Caminho para o plantel do utilizador
 
-            String leagueFile = "LigaPortugal.json"; // <- ajusta se necessário
-            String clubsFile = "clubs.json";
-            String squadsFile = "squad.json";
-            String mySquadFile = "mySquad.json";
-            // Importar ligas do ficheiro JSON
-           
-            
-            
-            
-          
-           
+            // Importar clubes e plantéis do ficheiro JSON
             Team[] totalClubs = TeamImporterJSON.teamsFromJson(clubsFile);
             Squad[] totalSquads = TeamImporterJSON.squadsFromJson(squadsFile, totalClubs);
-            //Squad do utilizador num ficheiro aparte
+
+            // Squad do utilizador num ficheiro à parte
             Squad mySquad = null;
             File fileMySquad = new File(mySquadFile);
-            
-            if(fileMySquad.exists()){
+
+            if (fileMySquad.exists()) {
                 mySquad = TeamImporterJSON.mySquadFromJson(mySquadFile, totalClubs);
             }
-            
-      
 
-
-
-      
+            // Importar a liga e obter a primeira época
             League league = LeagueImporterJSON.readLeagueFromFile(leagueFile);
             ISeason s1 = league.getSeason(0);
-        
-            
-           
-             ((Season)s1).generateMatchesAutomatically();
-            
-            s1.generateSchedule();
-            ((Season)s1).setMyTeam(mySquad);
-            
-            league.setFile(leagueFile);
-            // Inicializar estratégia de simulação e simulador
-            MatchSimulatorStrategy strategy = new DefaultMatchSimulator();
-            
-            LeagueSimulator simulator = new LeagueSimulator(league);
-            
-            
 
-            // Iniciar menu principal
+            // Gerar jogos e definir equipa do utilizador
+            ((Season) s1).generateMatchesAutomatically();
+            s1.generateSchedule();
+            ((Season) s1).setMyTeam(mySquad);
+
+            league.setFile(leagueFile); // Guardar o caminho do ficheiro associado à liga
+
+            // Inicializar estratégia e simulador
+            MatchSimulatorStrategy strategy = new DefaultMatchSimulator();
+            LeagueSimulator simulator = new LeagueSimulator(league);
+
+            // Iniciar o menu principal
             MainMenu menu = new MainMenu();
-            menu.mostrarMenu(mySquad,mySquadFile,totalSquads,league ,simulator, strategy,totalSquads,totalClubs);
+            menu.mostrarMenu(mySquad, mySquadFile, totalSquads, league, simulator, strategy, totalSquads, totalClubs);
 
         } catch (IOException e) {
             System.out.println("Erro ao iniciar o jogo: " + e.getMessage());
-            e.printStackTrace();  // <-- imprime onde o erro aconteceu no código
-            
+            e.printStackTrace(); // Imprime o stack trace para debug
         }
     }
 }
